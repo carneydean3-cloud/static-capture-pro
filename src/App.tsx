6e7f60412b873c4cf8f2e8a4dae269b4920454f8
carneydean3-cloud <carneydean3@gmail.com>
2026-04-06 18:13:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { CurrencyProvider } from "@/contexts/CurrencyContext";
 import { AuditProvider } from "@/contexts/AuditContext";
 import Index from "./pages/Index.tsx";
 import PaidReport from "./pages/PaidReport.tsx";
+import ReportById from "./pages/ReportById.tsx";
 import SubscriptionSuccess from "./pages/SubscriptionSuccess.tsx";
+import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Terms from "./pages/Terms.tsx";
 import Privacy from "./pages/Privacy.tsx";
@@ -26,6 +29,9 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/paid-report" element={<PaidReport />} />
+              <Route path="/report/:id" element={<ReportById />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/subscription-success" element={<SubscriptionSuccess />} />
               <Route path="/terms" element={<Terms />} />
               <Route path="/privacy" element={<Privacy />} />
